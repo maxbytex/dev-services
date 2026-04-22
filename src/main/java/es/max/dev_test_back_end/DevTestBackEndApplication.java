ad3c45f57c3b0546398c1bcdfa46c287d98d6fd3
maxxprogramming <131193399+maxxprogramming@users.noreply.github.com>
2026-04-22 20:46:10 +0200

    init config sprinboot starter

---
+++ src/main/java/es/max/dev_test_back_end/DevTestBackEndApplication.java
@@ -0,0 +1,13 @@
+package es.max.dev_test_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTestBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTestBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/max/dev_test_back_end/DevTestBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package es.max.dev_test_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTestBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
